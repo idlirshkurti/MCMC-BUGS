685c2474a37382c32e101b9e5d97affc3a7ceb2c
Idlir Shkurti <idlir.sh@gmail.com>
2018-09-09 14:04:21 +0200

    Update packages.R

--- packages.R
+++ packages.R
@@ -1,2 +1,3 @@
+# Call this from all the other scripts in order to load libraries
 packages <- c("ggplot2", "gridExtra", "reshape", "MCMCpack", "lattice", "rjags", "mcmcplots")
 lapply(packages, require, character.only = TRUE)
